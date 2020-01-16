db87532535c3f4adaf8a8b238f92ef2eba32f3d7
Carlos Salazar <carlosiusalazar@gmail.com>
2020-01-16 18:45:32 +0900

    warm up week1class2 finished

--- Week1Class2/Warm Up/script.js
+++ Week1Class2/Warm Up/script.js
@@ -1,3 +1,26 @@
 'use strict'
 // Please don't delete the 'use strict' line above
 
+//1 You will use a new JavaScript operator for this: typeof. You can learn more about this operator here.
+
+const variableString = "I am a string";
+console.log(typeof variableString); // should print "string"
+
+const variableNumber = 123;
+console.log(typeof variableNumber); // What goes here? // should print "number"
+
+// Your code here
+let variableBoolean = true;
+console.log(typeof variableBoolean); // should print "boolean"
+
+let variableUndefined = undefined;
+console.log(typeof variableUndefined); // should print "undefined"
+
+
+// 2 Compute and store the average of two numbers.
+
+const numberA = 34;
+const numberB = 54;
+
+const average = (numberA + numberB)/2;
+console.log(average); // should print the average of numberA and numberB
