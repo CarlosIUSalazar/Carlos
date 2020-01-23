ec85217758bd1512a2bbd4afda89a790cf61f988
Carlos Salazar <carlosiusalazar@gmail.com>
2020-01-23 18:43:03 +0900

    Wk 2 Cl 2 Warmup Add

---
+++ Week 2/Week 2 Class 2 23 Jan 2020/TDD-Warm-Up/script.js
@@ -0,0 +1,41 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+// 1 Declare a function compareTwoNumbers.
+
+// Arguments
+
+// (number): the first number
+// (number): the second number
+// Returns
+
+// (string): an explanation of how the given numbers compare. There are 3 possibilities:
+
+// “X is equal to Y”
+// “X is less than Y”
+// “X is greater than Y”
+// Declare your function here.
+
+let actual = compareTwoNumbers(1, 1);
+let expected = "1 is equal to 1";
+
+if (actual === expected) {
+  console.log("Yay! Test PASSED.");
+} else {
+  console.error("Test FAILED. Keep trying!");
+  console.log("    actual: ", actual);
+  console.log("  expected: ", expected);
+}
+
+actual = compareTwoNumbers(3, 4);
+expected = "3 is less than 4";
+
+if (actual === expected) {
+  console.log("Yay! Test PASSED.");
+} else {
+  console.error("Test FAILED. Keep trying!");
+  console.log("    actual: ", actual);
+  console.log("  expected: ", expected);
+}
+
+// Write your own test for "X is greater than Y".
