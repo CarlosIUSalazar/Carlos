cbaf5fda1f0a4cd28b3f01093b8bad63ec71b992
Carlos Salazar <carlosiusalazar@gmail.com>
2020-01-28 18:46:37 +0900

    Warmup For of Week 3 Complete

--- Week 3/Wk3 Class 1 Jan 28/WarmUp-For-Of-Loops/script.js
+++ Week 3/Wk3 Class 1 Jan 28/WarmUp-For-Of-Loops/script.js
@@ -1,3 +1,39 @@
 'use strict'
 // Please don't delete the 'use strict' line above
 
+// 1 Declare a function getOddNumbers.
+
+// Arguments
+
+// ([number]): an array of numbers
+// Returns
+
+// ([number]): an array of all the odd numbers in the given array
+
+function getOddNumbers(arr) {
+    // Your code here.
+    let newArr = [];
+
+    for (let item of arr){
+        if (item % 2 !== 0){
+            newArr.push(item);
+        }
+    }
+
+    return newArr;
+
+  }
+  
+  let actual = getOddNumbers([1, 2, 3, 4, 5, 6, 7, 8]);
+  let expected = [1, 3, 5, 7];
+  
+  // Remember, we need to use JSON.stringify to compare arrays (for now)
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log("Yay! Test PASSED.");
+  } else {
+    console.error("Test FAILED. Keep trying!");
+    console.log("    actual: ", actual);
+    console.log("  expected: ", expected);
+  }
+  
+  // Write more tests
