d6b237b2dc36e892301248d62317a97f17c060a2
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-04 15:33:40 +0900

    Data Modeling Movie App

--- Week 3/Wk3 Class 3 Feb 1/Data-Modeling-Intro/script.js
+++ Week 3/Wk3 Class 3 Feb 1/Data-Modeling-Intro/script.js
@@ -34,7 +34,7 @@ function makeMovie(title,director,year,length,rating){
         title:title,
         director:director,
         year:year,
-        "Length of film in minutes" : length,
+        lengthOfFilm : length,
         rating,rating
     }
 
@@ -49,11 +49,11 @@ function makeMovie(title,director,year,length,rating){
 
 // 3 Make five more movie objects using your factory function and store them in an array called movies.
 
-let movie1 = makeMovie("The Lord of The Rings", "Tolkien", 2002, "180", "R");
-let movie2 = makeMovie("Batman", "Peter Dinklage", 1997, "120", "PG-13");
-let movie3 = makeMovie("Star Wars", "George Lucas", 1977, "90", "PG");
-let movie4 = makeMovie("Once upon a time in Hollywood", "Quentin Tarantino", 2019, "140", "R");
-let movie5 = makeMovie("Terminator 2", "James Cameron", 1991, "120", "R");
+let movie1 = makeMovie("The Lord of The Rings", "Tolkien", 2002, 180, "R");
+let movie2 = makeMovie("Batman", "Peter Dinklage", 1997, 120, "PG-13");
+let movie3 = makeMovie("Star Wars", "George Lucas", 1977, 90, "PG");
+let movie4 = makeMovie("Once upon a time in Hollywood", "Quentin Tarantino", 2019, 140, "R");
+let movie5 = makeMovie("Terminator 2", "James Cameron", 1991, 120, "R");
 
 console.log(movie1)
 console.log(movie2)
@@ -65,7 +65,7 @@ console.log(movie5)
 // 4 Declare a function displayMovie that works like displayBook, but for movies.
 
 function displayMovie(movie){
-    console.log(movie.title + " by " + movie.director +". Released in " + movie.year + ". Length in Minutes: " + movie["Length of film in minutes"] +". Rating: " + movie.rating + ".")
+    console.log(movie.title + " by " + movie.director +". Released in " + movie.year + ". Length in Minutes: " + movie.lengthOfFilm +". Rating: " + movie.rating + ".")
 }
 
 console.log(displayMovie(movie2));
@@ -81,9 +81,32 @@ console.log("movieArray is", movieArray);
 
 function averageLength(movies){
 
+    let averageLength = 0;
+    let movieLengths = [];
 
+    for (let item of movies){
+        for (const key in item){
+            //console.log("key", key)
+            if (key === "lengthOfFilm"){
+                //console.log("push ", item[key])
+                movieLengths.push(item[key])
+            }
+        }
+    }
+    //console.log("movieLengths are ", movieLengths);
+
+    let totalDuration = 0;
+
+    for(let item of movieLengths){
+        totalDuration += item;
+    }
+
+    averageLength = totalDuration/movieLengths.length
+
+    //console.log("averageLength is ", averageLength);
+    return averageLength
 
 }
 
-
+console.log("What is the total average lenths of my movies: ", averageLength(movieArray),"mins.");
 // 6 Declare a function averageRating that accepts an array of movies and returns the average rating.
