4f4aca44d6460496519db981715ed8cf434ec75f
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-06 20:29:10 +0900

    Methods Added Medium Requeriments Complete

--- Week 4/Week 4 Class 2 Feb 6th/Methods/script.js
+++ Week 4/Week 4 Class 2 Feb 6th/Methods/script.js
@@ -58,6 +58,49 @@ const myMathObject = {
         return result;
 
      },
+
+     getNumbers: function (...args){
+
+        let returnArray = [];
+
+        for (let item of args){
+            if (typeof(item) === "number")
+            returnArray.push(item);
+        }
+
+        console.log(returnArray)
+        return returnArray;
+
+     },
+
+     abs: function (number){
+
+        let returnValue = 0;
+
+         if (number > 0){
+             return number
+         } else if (number < 0){
+
+            returnValue = -number;
+            console.log("number ",number);
+            console.log("returnValue", returnValue);
+            return returnValue;
+         
+        }
+         return returnValue;
+     },
+
+     power: function(base,exponent){
+
+        let result = 1;
+
+        result = base**exponent;
+
+        return result;
+     }
+
+    
+
 };
 
 
@@ -91,6 +134,88 @@ test(myMathObject.isEven(2), true);
 test(myMathObject.isEven(19), false);
 
 
+
+// 3 In the above object, create a method called getNumbers.
+
+// Arguments
+
+// (…*): an arbitrary number of arguments
+// Returns
+
+// ([number]): all of the given arguments that are numbers
+
+test(myMathObject.getNumbers(1, 2, 3, 4), [1, 2, 3, 4]);
+test(myMathObject.getNumbers("foo", 3, "4", "hi", 1), [3, 1]);
+
+
+
+
+// Medium Requirements
+// Add a method abs to myMathObject. You should not use any built-in Math methods.
+
+// Arguments
+
+// (number)
+// Returns
+
+// (number): the absolute value of the given number;
+
+test(myMathObject.abs(1), 1);
+test(myMathObject.abs(-1), 1);
+test(myMathObject.abs(435678.745389), 435678.745389);
+test(myMathObject.abs(-675843.753489), 675843.753489);
+
+
+
+// 2 Add a method power to myMathObject. You should not use the built-in Math.pow(), 
+// except for testing.
+
+// Arguments
+
+// (number): base
+// (number): exponent
+// Returns
+
+// (number): the result of raising the given base to the given exponent
+
+const MIN_BASE = -5;
+const MAX_BASE = 5;
+const MIN_EXPONENT = -5;
+const MAX_EXPONENT = 5;
+
+// Run many tests with few lines of code.
+// This test uses extra code for floating-point number comparison
+for (let base = MIN_BASE; base <= MAX_BASE; ++base) {
+  for (let exponent = MIN_EXPONENT; exponent <= MAX_EXPONENT; ++exponent) {
+    const actual = myMathObject.power(base, exponent);
+    const expected = Math.pow(base, exponent);
+    if (isNearlyEqual(actual, expected)) {
+      console.log("Yay! Test PASSED.");
+    } else {
+      console.error("Test FAILED. Keep trying!");
+      console.log("    actual: ", actual);
+      console.log("  expected: ", expected);
+      console.trace();
+    }
+  }
+}
+
+// Credit: adapted from https://floating-point-gui.de/errors/comparison/
+function isNearlyEqual(a, b) {
+  const absA = Math.abs(a);
+  const absB = Math.abs(b);
+  const diff = Math.abs(a - b);
+
+  if (a == b) {
+    return true;
+  } else if (a == 0 || b == 0 || absA + absB < Number.MIN_VALUE) {
+    return diff < Number.EPSILON * Number.MIN_VALUE;
+  } else {
+    return diff / Math.min(absA + absB, Number.MAX_VALUE) < Number.EPSILON;
+  }
+}
+
+
 function test(actual, expected) {
     if (JSON.stringify(actual) === JSON.stringify(expected)) {
         console.log("Yay! Test PASSED.");
@@ -101,3 +226,6 @@ function test(actual, expected) {
         console.trace();
     }
 }
+
+
+
