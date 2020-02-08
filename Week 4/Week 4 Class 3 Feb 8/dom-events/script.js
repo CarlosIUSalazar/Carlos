80d0ced579aa8c952c2082f3ebd6e68c212e3ce9
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-08 14:08:24 +0900

    DOM Events Complete

---
+++ Week 4/Week 4 Class 3 Feb 8/dom-events/script.js
@@ -0,0 +1,25 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+//Modifying www.codechrysalis.io
+
+let h1 = document.getElementsByTagName("h1")[0]
+
+
+h1.style.backGroundColor = "green"
+
+
+h1.style.color = "green"
+
+
+h1.style.textAlign = "left"
+
+
+h1.style.color = "black"
+
+
+const h2element = document.createElement("h2");
+
+h2element.innerText = "Starring Carlos"
+
+h1.appendChild(h2element)
