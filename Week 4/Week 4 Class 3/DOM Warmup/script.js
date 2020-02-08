cc8a2976f0af8584e3cf6e17b5f282e56e6608c9
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-08 09:41:08 +0900

    DOM Warmup Complete Feb 8 2020

--- Week 4/Week 4 Class 3/DOM Warmup/script.js
+++ Week 4/Week 4 Class 3/DOM Warmup/script.js
@@ -13,3 +13,39 @@
 // Change the first <h3> element’s innerHTML to "I am a <i>fancy</i> h3 element".
 
 
+//let image = document.querySelectorAll("img");
+
+//let element = document.getElementById('unicorn').setAttribute(width,'500')
+// element.height = 150;
+// element.width = 150;
+//console.log(element)
+// element.setAttribute(heigth,500)
+
+// let text = document.querySelectorAll('h1');
+// text[0].innertext = "I am the first h1 element"
+
+
+// document.getElementById("firsth1").innerHTML = "yourTextHere";
+
+
+// let image = document.getElementsByTagName('img')
+
+//console.log(image)
+
+
+// PUT THE SCRIPT
+
+document.getElementsByTagName('img')[0].style = "width:500px"
+
+document.getElementsByTagName('img')[0].style = "width:600px, opacity:0.5"
+
+document.getElementsByTagName("h1")[0].innerText = "I am the h1 tag"
+
+document.getElementsByTagName("h2")[1].innerText = "I am the h1 tag"
+
+//innertext vs innerHTML
+
+document.getElementsByTagName("h2")[1].innerHTML = "I am <i>Fancy</i> h1 tag"
+
+
+document.getElementsByTagName("h2")[1].children[0].innerText = "Children"
