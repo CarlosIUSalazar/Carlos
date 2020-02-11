1558d837d035a757b73c0a23e501865e0a83ba19
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-11 20:58:57 +0900

    Medium Advanced Complete

--- Week 5/Wk5Class1/HOFS Intro/script.js
+++ Week 5/Wk5Class1/HOFS Intro/script.js
@@ -70,13 +70,7 @@ test([-1, 2, -3].map(abs), [1, 2, 3]);
 
 function getIncrementedNumbers(array){
 
-    let newArray = array.map(x => x+= 1);
-
-
-
-    // for (let i =0; i < array.length; i++){
-    //     newArray.push(array[i]+1);
-    // }
+    let newArray = array.map(x => x += 1);
 
     return newArray
 
@@ -89,3 +83,112 @@ const array1 = [0, 0, 0];
 test(getIncrementedNumbers(array1), [1, 1, 1]);
 // Make sure that the original array is untouched
 test(array1, [0, 0, 0]);
+
+
+// Medium Requirements
+// 1 Declare a function getSwitched. This function should call .map. You may need to write a helper function, or you can use an anonymous function.
+
+// Arguments
+
+// ([number|string])
+// Returns
+
+// ([number|string]): a new array, with each number converted to a string, and each string converted to a number
+
+// Your code here
+
+function getSwitched(array){
+
+    let newArray = array.map(function(x){ 
+        if(typeof x === "number"){
+            return String(x);
+        } else if (typeof x === "string"){
+            return Number(x);
+        }
+    })
+    return newArray;
+
+}
+
+test(getSwitched([1, 2, 3]), ["1", "2", "3"]);
+test(getSwitched(["1", "2", "3"]), [1, 2, 3]);
+test(getSwitched(["1", 2, "3"]), [1, "2", 3]);
+
+
+
+// Advanced Requirements
+// You may need to use techniques that we didn’t go over in class for these exercises
+
+// Declare a function multiplyByIndex.
+
+// Arguments
+
+// ([number])
+// Returns
+
+// ([number]): a new array, with each number multiplied by it’s index in the given array
+
+// Your code here
+
+function multiplyByIndex(array){
+
+    let returnArray = [];
+    let index = 0
+
+    returnArray = array.map(function(x){
+            x = x * index;
+            index++
+        return x
+
+    })
+    return returnArray;
+}
+
+test(multiplyByIndex([1, 2, 3]), [0, 2, 6]);
+test(multiplyByIndex([2, 3, 4]), [0, 3, 8]);
+test(multiplyByIndex([-3, -4, -5]), [-0, -4, -10]);
+// -0 ??? JavaScript is weird.
+
+
+
+
+// 2 Declare a function feedback.
+
+// Arguments
+
+// ([*])
+// (function): a function to perform on the given array
+// (number): the number of times to perform the given function on the array
+// Returns
+
+// ([*]): a new array. Each element has been run through the given function the given number of times
+
+//debugger;
+// Your code here
+function double(element) {
+    return element * 2;
+  }
+  
+
+
+function feedback(array,func,number){
+
+    let returnArray = [];
+    let counter = 0;
+    
+    while (counter < number){
+        returnArray = array.map(x => func(x))
+        counter++;
+    }
+
+    return returnArray;
+
+}
+
+
+
+
+
+//test(feedback([1, 2, 3], double, 1), [2, 4, 6]);
+test(feedback([1, 2, 3], double, 2), [4, 8, 12]);
+test(feedback([1, 2, 3], double, 8), [256, 512, 768]);
