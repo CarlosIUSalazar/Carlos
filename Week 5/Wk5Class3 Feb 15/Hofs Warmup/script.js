1b88fb83d07f9295ee1a4e28c79b3caff4c7e4ce
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-19 10:29:23 +0900

    Refactor

--- Week 5/Wk5Class3 Feb 15/Hofs Warmup/script.js
+++ Week 5/Wk5Class3 Feb 15/Hofs Warmup/script.js
@@ -11,15 +11,12 @@ function doTheThing(arr){
     // let newArray = [];
     // let newArray = arr.filter(element => element <= 5)
     // newArray = arr.filter(element => element < 6).map(element => element*element)
-
     // return newArray;
-
     return arr
             .filter(num => num <= 5)
             .map((num) => num*num)
 
 }
-
 
 test(doTheThing([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [1, 4, 9, 16, 25]);
 test(doTheThing([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), [25, 16, 9, 4, 1]);
